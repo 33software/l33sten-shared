5f366e7a7c75db8fe614c1f2224e5df8c38f0125
gxddem <gxddem@gmail.com>
2025-04-29 18:43:50 +0300

    v0.0.8 - error interceptor

---
+++ interceptors/errors/error_interceptor.go
@@ -0,0 +1,38 @@
+package interceptors
+
+import (
+	"context"
+	"errors"
+
+	"github.com/33software/l33sten-shared/sharederr"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func UnrayErrorInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, request any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		resp, err := handler(ctx, request)
+
+		return resp, ErrToGRPCStatus(err)
+		
+}}
+
+func ErrToGRPCStatus (err error) error {
+	if err == nil {
+		return nil
+	}
+
+	if status, ok := status.FromError(err); ok {
+		return status.Err()
+	}
+
+	switch {
+	case errors.Is(err, sharederr.ErrDuplicate):
+		return status.Error(codes.AlreadyExists, err.Error())
+	case errors.Is(err, sharederr.ErrNotFound):
+		return status.Error(codes.NotFound, err.Error())
+	default:
+		return status.Error(codes.Internal, "internal server error")
+	}
+}
---
+++ sharederr/errors.go
@@ -0,0 +1,8 @@
+package sharederr
+
+import "errors"
+var (
+ErrNotFound = errors.New("not found")
+ErrDuplicate = errors.New("duplicate")
+
+)
